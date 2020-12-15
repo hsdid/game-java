0118d8622fc7b5769e1bf59816b7a456835a94a4
hsdid <murawskimat00@gmail.com>
2020-12-15 17:35:59 +0100

    Update Enemy move method

--- src/com/company/Enemy.java
+++ src/com/company/Enemy.java
@@ -8,19 +8,25 @@ public class Enemy extends GameObject{
     private int health;
     Handler handler;
     private GameObject player;
+    private GameObject player2;
+
     private int width=20, height=32;
 
     public Enemy(float x, float y, ID id, Handler handler) {
         super(x, y, id);
 
         this.handler = handler;
-        this.health = 4;
-        //get player rom object list
+        this.health  = 4;
+
+        //get player from object list
         for (int i = 0; i < handler.object.size(); i++){
             if (handler.object.get(i).getId() == ID.Player)
-                player = handler.object.get(i);
-        }
+                player  = handler.object.get(i);
 
+            if (handler.object.get(i).getId() == ID.Player2) {
+                player2 = handler.object.get(i);
+            }
+        }
     }
 
 
@@ -29,13 +35,11 @@ public class Enemy extends GameObject{
         x += speedX;
         y += speedY;
 
-        float diffX = x - player.getX() - 8;
-        float diffY = y - player.getY() - 8;
-
-        float distance = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY()));
-
-        speedX = (float) ((-1.0/distance) * diffX);
-        speedY = (float) ((-1.0/distance) * diffY);
+        if ( player2 == null ){
+            enemyMoveOnePlayer();
+        } else {
+            enemyMove2Players();
+        }
 
         if (y <= 0 || y >= GameMain.HEIGHT - 32) speedY *= -1;
         if (x <= 0 || x >= GameMain.WIDTH - 32) speedX *= -1;
@@ -43,9 +47,46 @@ public class Enemy extends GameObject{
         collision();
 
 
+
+    }
+
+    private void enemyMoveOnePlayer(){
+        float diffX = x - player.getX() - 8;
+        float diffY = y - player.getY() - 8;
+
+        float distance = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY()));
+
+        speedX = (float) ((-1.0/distance) * diffX);
+        speedY = (float) ((-1.0/distance) * diffY);
+    }
+
+    private void enemyMove2Players(){
+        //distance Player 1
+        float diffX = x - player.getX() - 8;
+        float diffY = y - player.getY() - 8;
+        float distance = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY()));
+
+        //distance player2
+
+        float diffX2 = x - player2.getX() - 8;
+        float diffY2 = y - player2.getY() - 8;
+        float distance2 = (float) Math.sqrt( (x-player2.getX()) * (x-player2.getX()) + (y-player2.getY()) * (y-player2.getY()));
+
+        // go to playaer 1
+        if (distance <= distance2) {
+
+            speedX = (float) ((-1.0/distance) * diffX);
+            speedY = (float) ((-1.0/distance) * diffY);
+
+        } else { // else go to player 2
+
+            speedX = (float) ((-1.0/distance2) * diffX2);
+            speedY = (float) ((-1.0/distance2) * diffY2);
+        }
     }
 
     private void collision() {
+
         for (int i = 0; i < handler.object.size(); i++){
             GameObject tempObject = handler.object.get(i);
 
@@ -73,8 +114,6 @@ public class Enemy extends GameObject{
 
 
     public void render(Graphics g) {
-        //g.setColor(Color.PINK);
-        //g.fillRect((int)x,(int)y,20, 20);
 
         g.drawImage(Assets.zombie, (int)x,(int)y,width,height,null);
     }
--- src/com/company/StrongerEnemy.java
+++ src/com/company/StrongerEnemy.java
@@ -5,12 +5,16 @@ import java.awt.*;
 public class StrongerEnemy extends GameObject{
 
 
-    private int health;
-    Handler handler;
-    private GameObject player;
-    private float distance;
-    int timer = 40;
-    int wight = 30, height=40;
+    private int         health;
+    Handler             handler;
+    private GameObject  player;
+    private GameObject  player2;
+    private float       distance;
+    private float       distance2;
+
+    int                 timer = 40;
+    int                 wight = 30,height=40;
+
 
     public StrongerEnemy(float x, float y, ID id, Handler handler) {
         super(x, y, id);
@@ -21,6 +25,10 @@ public class StrongerEnemy extends GameObject{
         for (int i = 0; i < handler.object.size(); i++){
             if (handler.object.get(i).getId() == ID.Player)
                 player = handler.object.get(i);
+
+            if (handler.object.get(i).getId() == ID.Player2) {
+                player2 = handler.object.get(i);
+            }
         }
     }
 
@@ -30,12 +38,7 @@ public class StrongerEnemy extends GameObject{
         x += speedX;
         y += speedY;
 
-        float diffX = x - player.getX() - 8;
-        float diffY = y - player.getY() - 8;
-        distance = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY()));
-
-        speedX = (float) ((-1.0/distance) * diffX);
-        speedY = (float) ((-1.0/distance) * diffY);
+        moveEnemy();
 
         if (y <= 0 || y >= GameMain.HEIGHT - 32) speedY *= -1;
         if (x <= 0 || x >= GameMain.WIDTH - 32) speedX *= -1;
@@ -43,37 +46,131 @@ public class StrongerEnemy extends GameObject{
         collision();
 
         shoot();
-
-
     }
 
     public void shoot() {
+        if (player2 == null) {
 
+            distance = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY()));
 
-        if (distance <= 130) {
+            if (distance <= 150) {
 
-            timer ++;
+                timer ++;
 
-            speedX = 0;
-            speedY = 0;
+                speedX = 0;
+                speedY = 0;
 
-            if (timer >= 50){
+                if (timer >= 50){
 
-                float diffX = x - player.getX() - 8;
-                float diffY = y - player.getY() - 8;
-                float distance = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY()));
+                    float diffX = x - player.getX() - 8;
+                    float diffY = y - player.getY() - 8;
 
-                float toX = (float) ((-1.0/distance) * diffX);
-                float toY = (float) ((-1.0/distance) * diffY);
+                    float toX = (float) ((-1.0/distance) * diffX);
+                    float toY = (float) ((-1.0/distance) * diffY);
 
-
-                handler.addObject(new EnemyBullet(x, y, ID.EnemyBullet, handler,toX,toY));
-                timer = 0;
+                    handler.addObject(new EnemyBullet(x, y, ID.EnemyBullet, handler,toX,toY));
+                    timer = 0;
+                }
             }
+        }
+        else {
 
+            distance  = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY())); // distance from player1
+            distance2 = (float) Math.sqrt( (x-player2.getX()) * (x-player2.getX()) + (y-player2.getY()) * (y-player2.getY())); // distance from player2
+
+                    //if true shoot to player 1
+                    if (distance <= distance2 && distance <= 150){
+
+                        timer ++;
+                        speedX = 0;
+                        speedY = 0;
+
+                        if (timer >= 50){
+
+                            float diffX = x - player.getX() - 8;
+                            float diffY = y - player.getY() - 8;
+
+
+                            float toX = (float) ((-1.0/distance) * diffX);
+                            float toY = (float) ((-1.0/distance) * diffY);
+
+                            handler.addObject(new EnemyBullet(x, y, ID.EnemyBullet, handler,toX,toY));
+                            timer = 0;
+
+                        }
+                    }
+                    else if (distance >= distance2 && distance2 <= 150){
+
+                        timer ++;
+                        speedX = 0;
+                        speedY = 0;
+                        if (timer >= 50) {
+                            //shoot to player 2
+                            float diffX = x - player2.getX() - 8;
+                            float diffY = y - player2.getY() - 8;
+
+
+                            float toX = (float) ((-1.0 / distance2) * diffX);
+                            float toY = (float) ((-1.0 / distance2) * diffY);
+
+                            handler.addObject(new EnemyBullet(x, y, ID.EnemyBullet, handler, toX, toY));
+                            timer = 0;
+                        }
+                    }
+                }
         }
 
+
+
+    private void moveEnemy() {
+        if ( player2 == null ){
+
+            enemyMoveOnePlayer();
+
+        } else {
+            enemyMove2Players();
+
+        }
     }
+
+    private void enemyMoveOnePlayer(){
+
+        float diffX = x - player.getX() - 8;
+        float diffY = y - player.getY() - 8;
+
+        distance = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY()));
+
+        speedX = (float) ((-1.0/distance) * diffX);
+        speedY = (float) ((-1.0/distance) * diffY);
+    }
+
+    private void enemyMove2Players(){
+        //distance Player 1
+        float diffX = x - player.getX() - 8;
+        float diffY = y - player.getY() - 8;
+        float distance = (float) Math.sqrt( (x-player.getX()) * (x-player.getX()) + (y-player.getY()) * (y-player.getY()));
+
+        //distance player2
+
+        float diffX2 = x - player2.getX() - 8;
+        float diffY2 = y - player2.getY() - 8;
+        distance2 = (float) Math.sqrt( (x-player2.getX()) * (x-player2.getX()) + (y-player2.getY()) * (y-player2.getY()));
+
+        // go to playaer 1
+        if (distance <= distance2) {
+
+            speedX = (float) ((-1.0/distance) * diffX);
+            speedY = (float) ((-1.0/distance) * diffY);
+
+        } else { // else go to player 2
+
+            speedX = (float) ((-1.0/distance2) * diffX2);
+            speedY = (float) ((-1.0/distance2) * diffY2);
+        }
+    }
+
+
+
 
     private void collision() {
         for (int i = 0; i < handler.object.size(); i++){
